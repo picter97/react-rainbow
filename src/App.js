74075741800769940ea6ccd8ba4b63ac3bdabb96
picter <picter2esolv@gmail.com>
2023-03-07 21:41:07 -0500

    first push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import ColorBlock from './colorBlock'
+
+function App(){
+    let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+    return (
+        <div className="App">
+
+        </div>
+    )
+}
+
+
+export default App;
